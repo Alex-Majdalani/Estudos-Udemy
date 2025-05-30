9131f2ba210455800421edddf8b37111b8cfa7c6
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-05-30 14:00:02 -0300

    ajustei a hora para tempo real com setInterval e data

--- curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
+++ curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
@@ -1,33 +1,51 @@
 class CalcController {
   constructor() {
-    this._displayCalc = "0";
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
     this.currentDate;
     this.initialize();
   }
 
   initialize() {
-    let displayCalcEl = document.querySelector("#display");
-    let dateEl = document.querySelector("#data");
-    let timeEl = document.querySelector("#hora");
+    //this._dateEl.innerHTML = "30/05/2025";
+    //this._timeEl.innerHTML = "10:00";
+    setInterval(() => {
+      this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+      this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }, 1000);
+  }
 
-    displayCalcEl.innerHTML = "0";
-    dateEl.innerHTML = "30/05/2025";
-    timeEl.innerHTML = "10:00";
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    return (this._timeEl.innerHTML = value);
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    return (this._dateEl.innerHTML = value);
   }
 
   get displaycalc() {
-    return this._displayCalc;
+    return this._displayCalcEl.innerHTML;
   }
 
-  set displaycalc(valor) {
-    this._displayCalc = valor;
+  set displaycalc(value) {
+    return (this._displayCalcEl.innerHTML = value);
   }
 
   get currentDate() {
-    return this._currentDate;
+    return new Date();
   }
 
-  set currentDate(valor) {
-    this._dataAtual = valor;
+  set currentDate(value) {
+    return (this._currentDate = value);
   }
 }
