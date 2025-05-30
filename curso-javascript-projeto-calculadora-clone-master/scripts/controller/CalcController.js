91d8ae0f89cd67fe6002c00a6f65b400d87ddfb2
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-05-29 21:25:48 -0300

    estudo de objetos e metodos

--- curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
+++ curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
@@ -1,1 +1,25 @@
-class CalcController {}
+class CalcController {
+  constructor() {
+    this._displayCalc = "0";
+    this.currentDate;
+    this.initialize();
+  }
+
+  initialize() {}
+
+  get displaycalc() {
+    return this._displayCalc;
+  }
+
+  set displaycalc(valor) {
+    this._displayCalc = valor;
+  }
+
+  get currentDate() {
+    return this.currentDate;
+  }
+
+  set currentDate(valor) {
+    this._dataAtual = valor;
+  }
+}
