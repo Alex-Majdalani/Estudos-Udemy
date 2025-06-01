e6d76b9db0f44f049f449293ee9771bd1196c235
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-05-31 21:50:25 -0300

    estudo basico

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,1 +1,22 @@
-alert("hello word");
+//alert("hello word");
+Carro = {
+  marca: "Renault",
+  modelo: "Logan",
+  ipva: "valor('rb15')",
+  dimensoes: {
+    c: "4.250mm",
+    l: "1.735mm",
+    h: "1.525mm",
+  },
+};
+marca = Carro.marca;
+modelo = Carro.modelo;
+comprimento = Carro.dimensoes.c;
+
+executar = function () {
+  console.log(marca);
+  console.log(modelo);
+  console.log(comprimento);
+};
+
+executar();
