ea358a7538905e4f1f2e7cb2201096538b04c592
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-06-04 15:33:10 -0300

    criei um json com os valores do looping

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,4 +1,4 @@
-var nome = document.querySelector("#exampleInputName");
+/*var nome = document.querySelector("#exampleInputName");
 var genero = document.querySelectorAll(
   "#form-user-create [name=gender]:checked"
 );
@@ -7,17 +7,20 @@ var pais = document.querySelector("#exampleInputCountry");
 var email = document.querySelector("#exampleInputEmail1");
 var senha = document.querySelector("#exampleInputPassword1");
 var foto = document.querySelector("#exampleInputFile");
-var usuario = document.querySelector("#exampleInputAdmin");
+var usuario = document.querySelector("#exampleInputAdmin");*/
 
 var campos = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 campos.forEach(function (campos, index) {
   if (campos.name == "gender") {
     if (campos.checked === true) {
-      console.log("SIM", campos);
+      user[campos.name] = campos.value;
     }
   } else {
-    console.log("NÃO");
+    user[campos.name] = campos.value;
   }
 
   //console.log(campos.id, campos.name, campos.value, index);
 });
+
+console.log(user);
