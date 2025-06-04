a5af1a630f27d39b948c0c3b7786b83ed060e9a6
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-06-04 09:09:08 -0300

    função para buscar todos os campos no formulario (forEach)

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,17 +1,15 @@
 var nome = document.querySelector("#exampleInputName");
-
-var genero = document.querySelector("#exampleInputGenderM");
-
-var genero = document.querySelector("#exampleInputGenderF");
-
+var genero = document.querySelectorAll(
+  "#form-user-create [name=gender]:checked"
+);
 var nascimento = document.querySelector("#exampleInputBirth");
-
 var pais = document.querySelector("#exampleInputCountry");
-
 var email = document.querySelector("#exampleInputEmail1");
-
 var senha = document.querySelector("#exampleInputPassword1");
-
-var senha = documento.querySelector("#exampleInputPassword1");
-
 var foto = document.querySelector("#exampleInputFile");
+var usuario = document.querySelector("#exampleInputAdmin");
+
+var campos = document.querySelectorAll("#form-user-create [name]");
+campos.forEach(function (campos, index) {
+  console.log(campos);
+});
